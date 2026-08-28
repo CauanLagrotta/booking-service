106f5fb529081801122d37a9dc87c9549d6e3e59
CauanLagrotta <cauanlagrotta.dev@gmail.com>
2026-08-27 23:05:10 -0300

    fix(booking-service): add JWT header to PaymentFeignClient

--- src/main/java/com/cauanlagrotta/service/client/PaymentFeignClient.java
+++ src/main/java/com/cauanlagrotta/service/client/PaymentFeignClient.java
@@ -7,11 +7,14 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient("PAYMENT-SERVICE")
 public interface PaymentFeignClient {
 
   @PostMapping("/api/payments/create")
-  ResponseEntity<PaymentLinkResponse> createPaymentLink(@RequestBody BookingDTO booking, @RequestParam PaymentMethod paymentMethod);
+  ResponseEntity<PaymentLinkResponse> createPaymentLink(@RequestBody BookingDTO booking,
+                                                        @RequestParam PaymentMethod paymentMethod,
+                                                        @RequestHeader("Authorization") String jwt);
 }
