7034cc846f7c52e520b76f4d2fe4b61b04be6657
CauanLagrotta <cauanlagrotta.dev@gmail.com>
2026-09-06 21:52:38 -0300

    fix(booking): correct import path in CategoryFeignClient

--- src/main/java/com/cauanlagrotta/service/client/CategoryFeignClient.java
+++ src/main/java/com/cauanlagrotta/service/client/CategoryFeignClient.java
@@ -1,10 +1,11 @@
 package com.cauanlagrotta.service.client;
 
-import com.cauanlagrotta.service_offering.dto.CategoryDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import com.cauanlagrotta.dto.CategoryDTO;
 
 @FeignClient
 public interface CategoryFeignClient {
